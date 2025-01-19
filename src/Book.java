30346bebd82fe1ba6aa5e983fbfbf58c24b971d6
AGKolosov <ak60ru@gmail.com>
2025-01-19 16:27:53 +0300

    For verification

---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    private Author author;
+    private String title;
+    private int publicationYear;
+
+    public Book(Author author, String title, int publicationYear) {
+        this.author = author;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Task-1");
-
+        Author authorOne = new Author("Сергей", "Лукьяненко");
+        Author authorTwo = new Author("Дмитрий", "Глуховский");
+        Book bookOne = new Book(authorOne, "Ночной Дозор", 1998);
+        Book bookTwo = new Book(authorTwo, "Метро 2033", 2005);
+        System.out.println("Книга первая: " + bookOne.getTitle() + " от " + bookOne.getAuthor().getFirstName() + " " + bookOne.getAuthor().getLastName() + ", опубликована в " + bookOne.getPublicationYear());
+        System.out.println("Книга вторая: " + bookTwo.getTitle() + " от " + bookTwo.getAuthor().getFirstName() + " " + bookTwo.getAuthor().getLastName() + ", опубликована в " + bookTwo.getPublicationYear());
+        bookTwo.setPublicationYear(2007);
+        System.out.println("Книга вторая: " + bookTwo.getTitle() + " от " + bookTwo.getAuthor().getFirstName() + " " + bookTwo.getAuthor().getLastName() + ", переиздана в " + bookTwo.getPublicationYear());
     }
 }
